af3b0998f53d6d51e949523d1d397bcf8f3b6cfc
Martijn van de Rijdt <martijn@enketo.org>
2013-09-20 11:02:47 -0600

    added ass to grunt - closes #7

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,26 @@
+/*jshint node:true*/
 "use strict";
 
-module.exports = function(grunt) {
+module.exports = function( grunt ) {
   var js;
 
   // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig( {
+    pkg: grunt.file.readJSON( 'package.json' ),
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [ 'Gruntfile.js', 'src/js/**/*.js', '!src/js/extern.js' ]
+    },
     jasmine: {
       test: {
         src: 'src/**/*.js',
         options: {
           specs: 'tests/spec/*.js',
-          helpers: ['tests/utils/*.js', 'tests/mocks/*.js'],
+          helpers: [ 'tests/utils/*.js', 'tests/mocks/*.js' ],
           vendor: [
-            'lib/jquery.min.js', 
+            'lib/jquery.min.js',
             'lib/bootstrap.min.js',
             'lib/modernizr.min.js',
             'lib/xpath/build/xpathjs_javarosa.min.js',
@@ -22,11 +29,27 @@ module.exports = function(grunt) {
           ]
         },
       }
-    }
-  });
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: [ {
+          expand: true,
+          cwd: 'src/sass',
+          src: [ '**/*.scss', '!**/_*.scss' ],
+          dest: 'build/css',
+          ext: '.css'
+        } ]
+      }
+    },
+  } );
 
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks( 'grunt-contrib-jasmine' );
+  grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+  grunt.loadNpmTasks( 'grunt-contrib-sass' );
 
-  grunt.registerTask('test', ['jasmine']);
-  grunt.registerTask('default', ['test']);
+  grunt.registerTask( 'test', [ 'jasmine' ] );
+  grunt.registerTask( 'default', [ 'jshint', 'sass', 'test' ] );
 };
