90f2ffa754e2a2b617db37f3cd2fe235b17c9eae
Martijn van de Rijdt <martijn@enketo.org>
2013-10-09 17:30:38 -0600

    build the widgets scss file dynamically based on the content of /config.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,12 +19,12 @@ module.exports = function( grunt ) {
         options: {
           specs: 'test/spec/*.js',
           helpers: [ 'test/util/*.js', 'test/mock/*.js' ],
-          template: require('grunt-template-jasmine-requirejs'),
+          template: require( 'grunt-template-jasmine-requirejs' ),
           templateOptions: {
             requireConfig: {
               baseUrl: 'lib',
               paths: {
-                app:   '../src/js'
+                app: '../src/js'
               },
               shim: {
                 'xpath/build/xpathjs_javarosa': {
@@ -49,6 +49,10 @@ module.exports = function( grunt ) {
         },
       }
     },
+    prepWidgetSass: {
+      writePath: 'src/sass/_widgets.scss',
+      widgetConfigPath: 'config.json'
+    },
     sass: {
       dist: {
         options: {
@@ -65,10 +69,41 @@ module.exports = function( grunt ) {
     },
   } );
 
+
   grunt.loadNpmTasks( 'grunt-contrib-jasmine' );
   grunt.loadNpmTasks( 'grunt-contrib-jshint' );
   grunt.loadNpmTasks( 'grunt-contrib-sass' );
 
+  grunt.registerTask( 'prepWidgetSass', 'Preparing _widgets.scss dynamically', function( ) {
+    var widgetConfig, widgetFolderPath, widgetSassPath, widgetConfigPath,
+      config = grunt.config( 'prepWidgetSass' ),
+      widgets = grunt.file.readJSON( config.widgetConfigPath ).widgets,
+      content = '// Dynamically created list of widget stylesheets to import based on the content\r\n' +
+        '// based on the content of config.json\r\n\r\n';
+
+    widgets.forEach( function( widget ) {
+      if ( widget.indexOf( 'widget/' ) === 0 ) {
+        //strip require.js module name
+        widgetFolderPath = widget.substr( 0, widget.lastIndexOf( '/' ) + 1 );
+        //replace widget require.js path shortcut with proper path relative to src/js
+        widgetSassPath = widgetFolderPath.replace( /^widget\//, '../widget/' );
+        //create path to widget config file
+        widgetConfigPath = widgetFolderPath.replace( /^widget\//, 'src/widget/' ) + 'config.json';
+        grunt.log.writeln( 'widget config path: ' + widgetConfigPath );
+        //create path to widget stylesheet file
+        widgetSassPath += grunt.file.readJSON( widgetConfigPath ).stylesheet;
+      } else {
+        grunt.log.error( [ 'Expected widget path "' + widget + '" in config.json to be preceded by "widget/".' ] );
+      }
+      //replace this by a function that parses config.json in each widget folder to get the 'stylesheet' variable
+      content += '@import "' + widgetSassPath + '";\r\n';
+    } );
+
+    grunt.file.write( config.writePath, content );
+
+  } );
+
   grunt.registerTask( 'test', [ 'jasmine' ] );
-  grunt.registerTask( 'default', [ 'jshint', 'sass', 'test' ] );
+  grunt.registerTask( 'style', [ 'prepWidgetSass', 'sass' ] );
+  grunt.registerTask( 'default', [ 'jshint', 'prepWidgetSass', 'sass', 'test' ] );
 };
