bd1f211326a4ca08cd9483c759de78a09b928978
Martijn van de Rijdt <martijn@enketo.org>
2013-10-29 13:18:08 -0600

    moved form sample files, provided ability to add forms dynamically using a transformation service

--- app.js
+++ app.js
@@ -1,62 +1,90 @@
 requirejs.config( {
-  baseUrl: 'lib/',
-  paths: {
-    js: '../src/js',
-    widget: '../src/widget',
-    text: 'text/text',
-    xpath: 'xpath/build/xpathjs_javarosa',
-    config: '../config.json',
-    gmaps: 'http://maps.google.com/maps/api/js?v=3.exp&sensor=false&libraries=places&callback=gmapsLoaded',
-    "file-manager": "file-manager/src/file-manager"
-  },
-  shim: {
-    'xpath': {
-      exports: 'XPathJS'
+    baseUrl: '../lib/',
+    paths: {
+        js: '../src/js',
+        widget: '../src/widget',
+        text: 'text/text',
+        xpath: 'xpath/build/xpathjs_javarosa',
+        config: '../config.json',
+        gmaps: 'http://maps.google.com/maps/api/js?v=3.exp&sensor=false&libraries=places&callback=gmapsLoaded',
+        "file-manager": "file-manager/src/file-manager"
     },
-    'bootstrap': {
-      deps: [ 'jquery' ],
-      exports: 'jQuery.fn.bootstrap'
-    },
-    'widget/date/bootstrap-datepicker/js/bootstrap-datepicker': {
-      deps: [ 'jquery' ],
-      exports: 'jQuery.fn.datepicker'
-    },
-    'widget/time/bootstrap-timepicker/js/bootstrap-timepicker': {
-      deps: [ 'jquery' ],
-      exports: 'jQuery.fn.timepicker'
+    shim: {
+        'xpath': {
+            exports: 'XPathJS'
+        },
+        'bootstrap': {
+            deps: [ 'jquery' ],
+            exports: 'jQuery.fn.bootstrap'
+        },
+        'widget/date/bootstrap-datepicker/js/bootstrap-datepicker': {
+            deps: [ 'jquery' ],
+            exports: 'jQuery.fn.datepicker'
+        },
+        'widget/time/bootstrap-timepicker/js/bootstrap-timepicker': {
+            deps: [ 'jquery' ],
+            exports: 'jQuery.fn.timepicker'
+        }
     }
-  }
 } );
 
 define( 'modernizr', [ ], Modernizr );
 
 requirejs( [ 'jquery', 'modernizr', 'js/Form' ],
-  function( $, modernizr, Form ) {
-    var loadErrors, form;
+    function( $, modernizr, Form ) {
+        var loadErrors, form;
 
-    //if querystring touch=true is added, override Modernizr
-    if ( window.location.search.indexOf( 'touch=true' ) > 0 ) {
-      modernizr.touch = true;
-      $( 'html' ).addClass( 'touch' );
-    }
+        //if querystring touch=true is added, override Modernizr
+        if ( getURLParameter( 'touch' ) === 'true' ) {
+            modernizr.touch = true;
+            $( 'html' ).addClass( 'touch' );
+        }
 
-    form = new Form( 'form.jr:eq(0)', dataStr );
-    //for debugging
-    window.form = form;
-    //initialize form and check for load errors
-    loadErrors = form.init( );
-    if ( loadErrors.length > 0 ) {
-      alert( 'loadErrors: ', loadErrors.toString( ) );
-    }
+        //check if HTML form is hardcoded or needs to be retrieved
+        if ( getURLParameter( 'xform' ) !== 'null' ) {
+            $( '.guidance' ).remove( );
+            $.get( 'http://xslt-dev.enketo.org/?xform=' + getURLParameter( 'xform' ), function( data ) {
+                var $data = $( data );
+                formStr = ( new XMLSerializer( ) ).serializeToString( $data.find( 'form:eq(0)' )[ 0 ] );
+                modelStr = ( new XMLSerializer( ) ).serializeToString( $data.find( 'model:eq(0)' )[ 0 ] );
+                $( '#validate-form' ).before( formStr );
+                initializeForm( );
+            }, 'xml' );
+        } else if ( $( 'form.jr' ).length > 0 ) {
+            $( '.guidance' ).remove( );
+            initializeForm( );
+        }
 
-    //validate handler for validate button
-    $( '#validate-form' ).on( 'click', function( ) {
-      form.validateForm( );
-      if ( !form.isValid( ) ) {
-        alert( 'Form contains errors. Please see fields marked in red.' );
-      } else {
-        alert( 'Form is valid! (see XML record in the console)' );
-        console.log( 'record:', form.getDataStr( ) );
-      }
+        //validate handler for validate button
+        $( '#validate-form' ).on( 'click', function( ) {
+            form.validateForm( );
+            if ( !form.isValid( ) ) {
+                alert( 'Form contains errors. Please see fields marked in red.' );
+            } else {
+                alert( 'Form is valid! (see XML record in the console)' );
+                console.log( 'record:', form.getDataStr( ) );
+            }
+            $
+        } );
+
+        //initialize the form
+
+        function initializeForm( ) {
+            form = new Form( 'form.jr:eq(0)', modelStr );
+            //for debugging
+            window.form = form;
+            //initialize form and check for load errors
+            loadErrors = form.init( );
+            if ( loadErrors.length > 0 ) {
+                alert( 'loadErrors: ', loadErrors.toString( ) );
+            }
+        }
+
+        //get query string parameter
+
+        function getURLParameter( name ) {
+            return decodeURI(
+                ( RegExp( name + '=' + '(.+?)(&|$)' ).exec( location.search ) || [ , null ] )[ 1 ]
+            );
+        }
     } );
-  } );
--- src/js/widgets.js
+++ src/js/widgets.js
@@ -1,201 +1,202 @@
 define( [ 'text!config', 'modernizr', 'jquery' ], function( config, modernizr, $ ) {
-  "use strict";
+    "use strict";
 
-  var $form = $( 'form.jr' ),
-    loaded = false,
-    globalConfig = JSON.parse( config ),
-    widgetConfig = [ ];
+    var $form,
+        loaded = false,
+        globalConfig = JSON.parse( config ),
+        widgetConfig = [ ];
 
-  /**
-   * load the widget modules (asynchronously)
-   *
-   * @param  {Function} callback
-   */
+    /**
+     * Initializes widgets
+     *
+     * @param  {jQuery} $group The element inside which the widgets have to be initialized.
+     */
 
-  function load( callback ) {
-    require( globalConfig.widgets, function( ) {
-      var id, widgetConfigFiles = [ ];
+    function init( $group ) {
+        $form = $( 'form.jr' );
+        $group = $group || $form;
 
-      console.log( 'widget modules loaded', arguments.length );
-
-      //add widget configuration to config object
-      for ( var i = 0; i < globalConfig.widgets.length; i++ ) {
-        id = 'text!' + globalConfig.widgets[ i ].substr( 0, globalConfig.widgets[ i ].lastIndexOf( '/' ) + 1 ) + 'config.json';
-        widgetConfigFiles.push( id );
-      }
-
-      //load widget config files
-      require( widgetConfigFiles, function( ) {
-        for ( var i = 0; i < arguments.length; i++ ) {
-          widgetConfig.push( JSON.parse( arguments[ i ] ) );
+        if ( !loaded ) {
+            load( function( ) {
+                create( $group );
+            } );
+        } else {
+            create( $group );
         }
-        console.log( 'widget config files loaded', widgetConfig );
-        loaded = true;
-        callback( );
-      } );
-    } );
-  }
-
-  /**
-   * Returns the elements on which to apply the widget
-   *
-   * @param  {jQuery} $group   a jQuery-wrapped element
-   * @param  {string} selector if the selector is null, the form element will be returned
-   * @return {jQuery}          a jQuery collection
-   */
-
-  function getElements( $group, selector ) {
-    return ( selector ) ? $group.find( selector ) : $form;
-  }
-
-  /**
-   * Initializes widgets
-   *
-   * @param  {jQuery} $group The element inside which the widgets have to be initialized.
-   */
-
-  function init( $group ) {
-    $group = $group || $form;
-
-    if ( !loaded ) {
-      load( function( ) {
-        create( $group );
-      } );
-    } else {
-      create( $group );
     }
-  }
 
-  /**
-   * Enables widgets if they weren't enabled already when the branch was enabled by the controller.
-   * In most widgets, this function will do nothing because the disabled attribute was automatically removed from all
-   * fieldsets, inputs, textareas and selects inside the branch element provided as parameter.
-   * Note that this function can be called before the widgets have been initialized and will in that case do nothing. This is
-   * actually preferable than waiting for create() to complete, because enable() will never do anything that isn't
-   * done during create().
-   *
-   * @param  {jQuery} $group [description]
-   */
+    /**
+     * load the widget modules (asynchronously)
+     *
+     * @param  {Function} callback
+     */
 
-  function enable( $group ) {
-    var widget, $els;
+    function load( callback ) {
+        require( globalConfig.widgets, function( ) {
+            var id, widgetConfigFiles = [ ];
 
-    console.debug( 'enabling widgets in ', $group );
+            console.log( 'widget modules loaded', arguments.length );
 
-    for ( var i = 0; i < widgetConfig.length; i++ ) {
-      widget = widgetConfig[ i ];
-      $els = getElements( $group, widget.selector );
-      $els[ widget.name ]( 'enable' );
+            //add widget configuration to config object
+            for ( var i = 0; i < globalConfig.widgets.length; i++ ) {
+                id = 'text!' + globalConfig.widgets[ i ].substr( 0, globalConfig.widgets[ i ].lastIndexOf( '/' ) + 1 ) + 'config.json';
+                widgetConfigFiles.push( id );
+            }
+
+            //load widget config files
+            require( widgetConfigFiles, function( ) {
+                for ( var i = 0; i < arguments.length; i++ ) {
+                    widgetConfig.push( JSON.parse( arguments[ i ] ) );
+                }
+                console.log( 'widget config files loaded', widgetConfig );
+                loaded = true;
+                callback( );
+            } );
+        } );
     }
-  }
 
-  /**
-   * Disables  widgets, if they aren't disabled already when the branch was disabled by the controller.
-   * In most widgets, this function will do nothing because all fieldsets, inputs, textareas and selects will get
-   * the disabled attribute automatically when the branch element provided as parameter becomes irrelevant.
-   *
-   * @param  { jQuery } $group The element inside which all widgets need to be disabled.
-   */
+    /**
+     * Returns the elements on which to apply the widget
+     *
+     * @param  {jQuery} $group   a jQuery-wrapped element
+     * @param  {string} selector if the selector is null, the form element will be returned
+     * @return {jQuery}          a jQuery collection
+     */
 
-  function disable( $group ) {
-    var widget, $els;
-
-    console.debug( 'disabling widgets in ', $group );
-
-    for ( var i = 0; i < widgetConfig.length; i++ ) {
-      widget = widgetConfig[ i ];
-      $els = getElements( $group, widget.selector );
-      $els[ widget.name ]( 'disable' );
+    function getElements( $group, selector ) {
+        return ( selector ) ? $group.find( selector ) : $form;
     }
-  }
 
-  /**
-   * Fixes deeply cloned widgets, if necessary. This function is only called with the repeat clone as a parameter.
-   * Many eventhandlers inside widgets get messed up when they are cloned. If so this function will have to fix
-   * that. In most widgets the destroy method will simple replace this.element with the cloned element.
-   *
-   * @param  {jQuery} $group The element inside which all widgets need to be fixed.
-   */
+    /**
+     * Enables widgets if they weren't enabled already when the branch was enabled by the controller.
+     * In most widgets, this function will do nothing because the disabled attribute was automatically removed from all
+     * fieldsets, inputs, textareas and selects inside the branch element provided as parameter.
+     * Note that this function can be called before the widgets have been initialized and will in that case do nothing. This is
+     * actually preferable than waiting for create() to complete, because enable() will never do anything that isn't
+     * done during create().
+     *
+     * @param  {jQuery} $group [description]
+     */
 
-  function destroy( $group ) {
-    var widget, $els;
+    function enable( $group ) {
+        var widget, $els;
 
-    for ( var i = 0; i < widgetConfig.length; i++ ) {
-      widget = widgetConfig[ i ];
-      $els = getElements( $group, widget.selector );
-      $els[ widget.name ]( 'destroy' );
+        console.debug( 'enabling widgets in ', $group );
+
+        for ( var i = 0; i < widgetConfig.length; i++ ) {
+            widget = widgetConfig[ i ];
+            $els = getElements( $group, widget.selector );
+            $els[ widget.name ]( 'enable' );
+        }
     }
-  }
 
-  /**
-   * Creates widgets upon initialization of the form or on a cloned element after having called 'destroy' first
-   *
-   * @param  {jQuery} $group The elements inside which widgets need to be created.
-   */
+    /**
+     * Disables  widgets, if they aren't disabled already when the branch was disabled by the controller.
+     * In most widgets, this function will do nothing because all fieldsets, inputs, textareas and selects will get
+     * the disabled attribute automatically when the branch element provided as parameter becomes irrelevant.
+     *
+     * @param  { jQuery } $group The element inside which all widgets need to be disabled.
+     */
 
-  function create( $group ) {
-    var widget, $els;
+    function disable( $group ) {
+        var widget, $els;
 
-    for ( var i = 0; i < widgetConfig.length; i++ ) {
-      widget = widgetConfig[ i ];
-      widget.options = widget.options || {};
-      widget.options.touch = modernizr.touch;
+        console.debug( 'disabling widgets in ', $group );
 
-      if ( !widget.name || ( !widget.selector && widget.selector !== null ) ) {
-        return console.error( 'widget configuration has no name and/or selector property', widget );
-      }
-
-      $els = getElements( $group, widget.selector );
-      $els[ widget.name ]( widget.options );
-
-      setLangChangeHandler( widget, $els );
-      setOptionChangeHandler( widget, $els );
+        for ( var i = 0; i < widgetConfig.length; i++ ) {
+            widget = widgetConfig[ i ];
+            $els = getElements( $group, widget.selector );
+            $els[ widget.name ]( 'disable' );
+        }
     }
-  }
 
-  /**
-   * Calls widget('update') when the language changes. This function is called upon initialization,
-   * and whenever a new repeat is created. In the latter case, since the widget('update') is called upon
-   * the elements of the repeat, there should be no duplicate eventhandlers.
-   *
-   * @param {{name: string}} widget The widget configuration object
-   * @param {jQuery}         $els   The jQuery collection of elements that the widget has been instantiated on.
-   */
+    /**
+     * Fixes deeply cloned widgets, if necessary. This function is only called with the repeat clone as a parameter.
+     * Many eventhandlers inside widgets get messed up when they are cloned. If so this function will have to fix
+     * that. In most widgets the destroy method will simple replace this.element with the cloned element.
+     *
+     * @param  {jQuery} $group The element inside which all widgets need to be fixed.
+     */
 
-  function setLangChangeHandler( widget, $els ) {
-    //call update for all widgets when language changes 
-    if ( $els.length > 0 ) {
-      $form.on( 'changelanguage', function( ) {
-        console.debug( 'change language event detected, going to update', widget.name );
-        $els[ widget.name ]( 'update' );
-      } );
+    function destroy( $group ) {
+        var widget, $els;
+
+        for ( var i = 0; i < widgetConfig.length; i++ ) {
+            widget = widgetConfig[ i ];
+            $els = getElements( $group, widget.selector );
+            $els[ widget.name ]( 'destroy' );
+        }
     }
-  }
 
-  /**
-   * Calls widget('update') on select-type widgets when the options change.This function is called upon initialization,
-   * and whenever a new repeat is created. In the latter case, since the widget('update') is called upon
-   * the elements of the repeat, there should be no duplicate eventhandlers.
-   *
-   * @param {{name: string}} widget The widget configuration object
-   * @param {jQuery}         $els   The jQuery collection of elements that the widget has been instantiated on.
-   */
+    /**
+     * Creates widgets upon initialization of the form or on a cloned element after having called 'destroy' first
+     *
+     * @param  {jQuery} $group The elements inside which widgets need to be created.
+     */
 
-  function setOptionChangeHandler( widget, $els ) {
-    if ( $els.length > 0 && $els.prop( 'nodeName' ).toLowerCase( ) === 'select' ) {
-      $form.on( 'changeoption', 'select', function( ) {
-        console.debug( 'option change detected, going to update', widget.name, 'for', $( this ) );
-        //update (itemselect) picker on which event was triggered because the options changed
-        $( this )[ widget.name ]( 'update' );
-      } );
+    function create( $group ) {
+        var widget, $els;
+
+        for ( var i = 0; i < widgetConfig.length; i++ ) {
+            widget = widgetConfig[ i ];
+            widget.options = widget.options || {};
+            widget.options.touch = modernizr.touch;
+
+            if ( !widget.name || ( !widget.selector && widget.selector !== null ) ) {
+                return console.error( 'widget configuration has no name and/or selector property', widget );
+            }
+
+            $els = getElements( $group, widget.selector );
+            $els[ widget.name ]( widget.options );
+
+            setLangChangeHandler( widget, $els );
+            setOptionChangeHandler( widget, $els );
+        }
     }
-  }
 
-  return {
-    init: init,
-    enable: enable,
-    disable: disable,
-    destroy: destroy
-  };
+    /**
+     * Calls widget('update') when the language changes. This function is called upon initialization,
+     * and whenever a new repeat is created. In the latter case, since the widget('update') is called upon
+     * the elements of the repeat, there should be no duplicate eventhandlers.
+     *
+     * @param {{name: string}} widget The widget configuration object
+     * @param {jQuery}         $els   The jQuery collection of elements that the widget has been instantiated on.
+     */
+
+    function setLangChangeHandler( widget, $els ) {
+        //call update for all widgets when language changes 
+        if ( $els.length > 0 ) {
+            $form.on( 'changelanguage', function( ) {
+                console.debug( 'change language event detected, going to update', widget.name );
+                $els[ widget.name ]( 'update' );
+            } );
+        }
+    }
+
+    /**
+     * Calls widget('update') on select-type widgets when the options change.This function is called upon initialization,
+     * and whenever a new repeat is created. In the latter case, since the widget('update') is called upon
+     * the elements of the repeat, there should be no duplicate eventhandlers.
+     *
+     * @param {{name: string}} widget The widget configuration object
+     * @param {jQuery}         $els   The jQuery collection of elements that the widget has been instantiated on.
+     */
+
+    function setOptionChangeHandler( widget, $els ) {
+        if ( $els.length > 0 && $els.prop( 'nodeName' ).toLowerCase( ) === 'select' ) {
+            $form.on( 'changeoption', 'select', function( ) {
+                console.debug( 'option change detected, going to update', widget.name, 'for', $( this ) );
+                //update (itemselect) picker on which event was triggered because the options changed
+                $( this )[ widget.name ]( 'update' );
+            } );
+        }
+    }
+
+    return {
+        init: init,
+        enable: enable,
+        disable: disable,
+        destroy: destroy
+    };
 
 } );
