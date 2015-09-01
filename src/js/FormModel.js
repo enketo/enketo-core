2ba58b7d92fbcebfb5cf59d6dcd6b2ca31c14c71
Alex Anderson <anderson@medicmobile.org>
2015-09-01 15:06:27 +0200

    Separate xpath evaluator binding into separate module
    This should make it simpler to stay in sync with `master`.

--- src/js/FormModel.js
+++ src/js/FormModel.js
@@ -8,8 +8,6 @@ define( function( require, exports, module ) {
     var MergeXML = require( 'mergexml/mergexml' );
     var utils = require( './utils' );
     var $ = require( 'jquery' );
-    var ExtendedXpathEvaluator = require('extended-xpath');
-    var openrosa_xpath_extensions = require( 'openrosa-xpath-extensions');
     require( './plugins' );
     require( './extend' );
     require( 'jquery-xpath-basic' );
@@ -281,42 +279,7 @@ define( function( require, exports, module ) {
      * Creates a custom XPath Evaluator to be used for XPath Expresssions that contain custom
      * OpenRosa functions or for browsers that do not have a native evaluator.
      */
-    FormModel.prototype.bindJsEvaluator = function() {
-        // re-implement XPathJS ourselves!
-        var evaluator = new XPathEvaluator();
-        this.xml.jsCreateExpression = function() {
-            return evaluator.createExpression.apply( evaluator, arguments );
-        };
-        this.xml.jsCreateNSResolver = function() {
-            return evaluator.createNSResolver.apply( evaluator, arguments );
-        };
-        this.xml.jsEvaluate = function(e, contextPath, namespaceResolver, resultType, result) {
-            var val, evaluator;
-            evaluator = new ExtendedXpathEvaluator(
-                    function wrappedXpathEvaluator(v) {
-                        var doc = contextPath.ownerDocument;
-                        return doc.evaluate(v, contextPath, namespaceResolver,
-                                // We pretty much always want to get a String in
-                                // the java rosa functions, and we don't want to
-                                // pass the top-level expectation all the way
-                                // down, so it's fairly safe to hard-code this,
-                                // especially considering we handle NUMBER_TYPEs
-                                // manually.
-                                // TODO what is `result` for?  Should it be
-                                // replaced in this call?
-                                XPathResult.STRING_TYPE, result);
-                                //resultType, result);
-                    },
-                    openrosa_xpath_extensions);
-            val = evaluator.evaluate(e);
-            return val;
-        };
-        window.JsXPathException =
-                window.JsXPathExpression =
-                window.JsXPathNSResolver =
-                window.JsXPathResult =
-                window.JsXPathNamespace = true;
-    };
+    FormModel.prototype.bindJsEvaluator = require('xpath-evaluator-binding');
 
     /**
      * Gets the instance ID
