9be89d7a37d23f64f68a1726f742ea19b6e02b12
Alex Anderson <anderson@medicmobile.org>
2015-09-01 12:10:25 +0200

    [medic-mobile] shim jquery

--- Gruntfile.js
+++ Gruntfile.js
@@ -114,7 +114,6 @@ module.exports = function( grunt ) {
             },
             options: {
                 alias: {},
-                exclude: [ 'jquery' ],
             },
         },
         uglify: {
