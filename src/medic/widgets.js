1ac6f05287dead38a35f18eb42846121f0dc97a2
Alex Anderson <anderson@medicmobile.org>
2015-09-01 15:33:32 +0200

    [medic-mobile] separate medic changes from core enketo code
    Medic changes to Enketo should now all be handled in separate modules from the
    enketo core code.  Use of Medic-specific modules is configured via grunt's
    `browserify:medic` target, and all medic-specific source files are moved to
    `/src/medic/`.

--- src/medic/widgets.js
+++ src/medic/widgets.js
@@ -5,21 +5,14 @@ if ( typeof exports === 'object' && typeof exports.nodeName !== 'string' && type
 }
 
 define( function( require, exports, module ) {
-    var widgets = [
+    widgets = [
         require( '../widget/note/notewidget' ),
-        require( '../widget/select-desktop/selectpicker' ),
         require( '../widget/select-mobile/selectpicker' ),
-        require( '../widget/geo/geopicker' ),
         require( '../widget/table/tablewidget' ),
         require( '../widget/radio/radiopicker' ),
         require( '../widget/date/datepicker-extended' ),
         require( '../widget/time/timepicker-extended' ),
         require( '../widget/datetime/datetimepicker-extended' ),
-        require( '../widget/mediagrid/mediagridpicker' ),
-        require( '../widget/file/filepicker' ),
-        require( '../widget/select-likert/likertitem' ),
-        require( '../widget/distress/distresspicker' ),
-        require( '../widget/horizontal-choices/horizontalchoices' )
     ];
 
     module.exports = widgets;
