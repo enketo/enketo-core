ae64b41b0b48799298e51529df78d66aa6334536
Martijn van de Rijdt <martijn@enketo.org>
2016-01-05 12:09:58 -0700

    changed: port used in headless karma tests

--- test/headless-karma.conf.js
+++ test/headless-karma.conf.js
@@ -47,7 +47,7 @@ module.exports = function( config ) {
 
 
         // web server port
-        port: 9876,
+        port: 9877,
 
 
         // enable / disable colors in the output (reporters and logs)
