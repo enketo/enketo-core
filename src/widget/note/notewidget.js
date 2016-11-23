fc9a6bc8f3202f9b5170c04cbd439665da2575ec
Martijn van de Rijdt <martijn@enketo.org>
2016-11-22 17:05:01 -0700

    fixed: notewidget unnecessarily reducing performance

--- src/widget/note/notewidget.js
+++ src/widget/note/notewidget.js
@@ -3,74 +3,11 @@ if ( typeof exports === 'object' && typeof exports.nodeName !== 'string' && type
         factory( require, exports, module );
     };
 }
-/**
- * @preserve Copyright 2012 Martijn van de Rijdt & Modilabs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
+var pluginName = 'notewidget';
 
 define( function( require, exports, module ) {
-    'use strict';
-    var Widget = require( '../../js/Widget' );
-    var $ = require( 'jquery' );
-    require( '../../js/plugins' );
-
-    var pluginName = 'notewidget';
-
-    /**
-     * Enhances notes
-     *
-     * @constructor
-     * @param {Element} element [description]
-     * @param {(boolean|{touch: boolean, repeat: boolean})} options options
-     * @param {*=} e     event
-     */
-
-    function Notewidget( element, options ) {
-        this.namespace = pluginName;
-        Widget.call( this, element, options );
-        this._init();
-    }
-
-    //copy the prototype functions from the Widget super class
-    Notewidget.prototype = Object.create( Widget.prototype );
-
-    //ensure the constructor is the new one
-    Notewidget.prototype.constructor = Notewidget;
-
-    Notewidget.prototype._init = function() {
-        // This widget is not doing anything at the moment. Leaving this in place for now.
-    };
-
-    Notewidget.prototype.destroy = function( element ) {};
-
-    $.fn[ pluginName ] = function( options, event ) {
-        return this.each( function() {
-            var $this = $( this ),
-                data = $this.data( pluginName );
-
-            options = options || {};
-
-            if ( !data && typeof options === 'object' ) {
-                $this.data( pluginName, new Notewidget( this, options, event ) );
-            } else if ( data && typeof options === 'string' ) {
-                data[ options ]( this );
-            }
-        } );
-    };
-
     module.exports = {
-        'name': pluginName,
-        'selector': '.note'
+        'name': pluginName
     };
 } );
