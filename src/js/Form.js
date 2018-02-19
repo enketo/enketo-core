f07725f1fd3b058b2bb3f219e49fee3ba49ee593
Martijn van de Rijdt <martijn@enketo.org>
2018-02-19 15:49:04 -0700

    fixed: test

--- src/js/Form.js
+++ src/js/Form.js
@@ -967,7 +967,7 @@ Form.getRequiredTransformerVersion = function() {
     console.deprecate( 'Form.getRequiredTransformerVersion()', 'Form.requiredTransformerVersion' );
     return Form.requiredTransformerVersion;
 };
-Form.requiredTransformerVersion = '1.24.1';
+Form.requiredTransformerVersion = '1.25.0';
 
 module.exports = Form;
 
