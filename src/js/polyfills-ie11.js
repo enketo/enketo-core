cae44b81f3e470687a9babb15d9b5e7eb377f113
Martijn van de Rijdt <martijn@enketo.org>
2018-07-25 15:55:10 -0600

    changed: refactored to make smaller non-IE11 builds,
    https://github.com/kobotoolbox/enketo-express/issues/1002

--- src/js/polyfills-ie11.js
+++ src/js/polyfills-ie11.js
@@ -55,18 +55,40 @@ if ( !Element.prototype.matches ) {
     window.CustomEvent = CustomEvent;
 } )();
 
+
+/**
+ * This is duplicate of the same function in the file-manager. This was done because:
+ * 1. The file-manager is normally overwritten in apps and urlToBlob was only used by the fileManager internally. 
+ *    Having a dependency in an IE11 polyfill file, means it would have to be added to the overriding file-manager just for IE11.
+ *    We'd like to avoid having any IE11 specific code in the main Enketo build.
+ * 2. We don't care about inefficiencies for IE-hacks, since in the future, an IE11-specific build will 
+ *    not be loaded by modern browsers (and we care only about peformance in modern browsers.)
+ */
+function urlToBlob( url ) {
+    var xhr = new XMLHttpRequest();
+
+    return new Promise( function( resolve ) {
+        xhr.open( 'GET', url );
+        xhr.responseType = 'blob';
+        xhr.onload = function() {
+            resolve( xhr.response );
+        };
+        xhr.send();
+    } );
+}
+
 // Replace download functionality for file upload and drawing widgets.
 ( function() {
     if ( window.navigator.msSaveOrOpenBlob && window.navigator.userAgent.indexOf( 'Trident/' ) >= 0 ) {
         var utils = require( './utils' );
-        var fileManager = require( 'enketo/file-manager' );
         var $ = require( 'jquery' );
 
         utils.updateDownloadLink = function( anchor, objectUrl, fileName ) {
             // Shut off / reset previous link
             $( anchor ).off( 'click' );
             if ( objectUrl ) {
-                fileManager.urlToBlob( objectUrl )
+
+                urlToBlob( objectUrl )
                     .then( function( blob ) {
                         $( anchor ).off( 'click' ).on( 'click', function() {
                             window.navigator.msSaveOrOpenBlob( blob, fileName );
