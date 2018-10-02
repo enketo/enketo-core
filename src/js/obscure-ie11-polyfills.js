2b8794a98a443557fdc154664db88f4257818134
Martijn van de Rijdt <martijn@enketo.org>
2018-10-02 13:31:55 -0600

    fixed: missing .after() polyfill for Comment nodes, closes #568

--- src/js/obscure-ie11-polyfills.js
+++ src/js/obscure-ie11-polyfills.js
@@ -66,3 +66,30 @@
 
     }
 }() );
+
+
+// I think the Financial Times polyfill does not include Comment Nodes
+//from: https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/after()/after().md
+( function( arr ) {
+    arr.forEach( function( item ) {
+        if ( item.hasOwnProperty( 'after' ) ) {
+            return;
+        }
+        Object.defineProperty( item, 'after', {
+            configurable: true,
+            enumerable: true,
+            writable: true,
+            value: function after() {
+                var argArr = Array.prototype.slice.call( arguments ),
+                    docFrag = document.createDocumentFragment();
+
+                argArr.forEach( function( argItem ) {
+                    var isNode = argItem instanceof Node;
+                    docFrag.appendChild( isNode ? argItem : document.createTextNode( String( argItem ) ) );
+                } );
+
+                this.parentNode.insertBefore( docFrag, this.nextSibling );
+            }
+        } );
+    } );
+} )( [ Element.prototype, CharacterData.prototype, DocumentType.prototype ] );
