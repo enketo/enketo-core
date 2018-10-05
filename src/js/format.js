dd7f810bc5fb17e85d080ba4ea162e891d50decb
Martijn van de Rijdt <martijn@enketo.org>
2018-10-05 15:18:06 -0600

    fixed: timepicker meridian detection not working in IE11, #566

--- src/js/format.js
+++ src/js/format.js
@@ -4,6 +4,15 @@ var TIME_PART = '[:' + NUMBER + ']+';
 var MERIDIAN_PART = '[^: ' + NUMBER + ']+';
 var HAS_MERIDIAN = new RegExp( '^(' + TIME_PART + ' ?(' + MERIDIAN_PART + '))|((' + MERIDIAN_PART + ') ?' + TIME_PART + ')$' );
 
+function _getCleanLocalTime( dt ) {
+    var d = typeof dt !== 'undefined' ? new Date( dt ) : new Date();
+    return _cleanSpecialChars( d.toLocaleTimeString( _locale ) );
+}
+
+function _cleanSpecialChars( timeStr ) {
+    return timeStr.replace( /[\u200E\u200F]/g, '' );
+}
+
 module.exports = {
     set language( lang ) {
         console.deprecate( 'format.language', 'format.locale' );
@@ -15,7 +24,7 @@ module.exports = {
     time: {
         // For now we just look at a subset of numbers in Arabic and Latin. There are actually over 20 number scripts and :digit: doesn't work in browsers
         get hour12() {
-            return HAS_MERIDIAN.test( new Date().toLocaleTimeString( _locale ) );
+            return this.hasMeridian( _getCleanLocalTime() );
         },
         get pmNotation() {
             return this.meridianNotation( '01-01-1970 23:00:00' );
@@ -24,7 +33,7 @@ module.exports = {
             return this.meridianNotation( '01-01-1970 01:00:00' );
         },
         meridianNotation: function( dt ) {
-            var matches = new Date( dt ).toLocaleTimeString( _locale ).match( HAS_MERIDIAN );
+            var matches = _getCleanLocalTime( dt ).match( HAS_MERIDIAN );
             if ( matches && matches.length ) {
                 matches = matches.filter( function( item ) { return !!item; } );
                 return matches[ matches.length - 1 ];
@@ -32,7 +41,7 @@ module.exports = {
             return null;
         },
         hasMeridian: function( time ) {
-            return HAS_MERIDIAN.test( time );
+            return HAS_MERIDIAN.test( _cleanSpecialChars( time ) );
         }
     }
 };
