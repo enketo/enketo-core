6432cc2f8a8b693257774d7a77166a7ad3b2cf31
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2019-03-25 21:11:32 +0100

    use and configure docdash theme

--- jsdoc.config.js
+++ jsdoc.config.js
@@ -8,16 +8,63 @@ module.exports = {
         recurse: true,
         package: 'package.json',
         readme: 'README.md',
-        verbose: true
+        template: 'node_modules/docdash'
     },
     plugins: ['plugins/markdown'],
     source: {
         include: [
-            'src'
+            'src/',
+            './README.md'
         ]
     },
     templates: {
         cleverLinks: true,
-        monospaceLinks: true
+        monospaceLinks: true,
+        default: {
+            outputSourceFiles: true,
+            includeDate: false,
+            useLongnameInNav: true
+        }
+    },
+    markdown: {
+        idInHeadings: true
+    },
+    docdash: {
+        static: true,
+        sort: true,
+        meta: {
+            title: 'Enketo Core',
+            description: 'Extensible Enketo form engine'
+        },
+        search: true,
+        collapse: true,
+        wrap: true,
+        typedefs: true,
+        removeQuotes: 'none',
+        scripts: [],
+        menu:{
+            'Github repo':{
+                href: 'https://github.com/enketo/enketo-core',
+                target: '_blank',
+                class: 'menu-item',
+                id: 'repository'
+            },
+            'Change log':{
+                href: 'https://github.com/enketo/enketo-core/blob/master/CHANGELOG.md',
+                target: '_blank',
+                class: 'menu-item',
+                id: 'change-log'
+            }
+        },
+        sectionOrder: [
+            'Tutorials',
+            'Classes',
+            'Modules',
+            'Externals',
+            'Events',
+            'Namespaces',
+            'Mixins',
+            'Interfaces'
+        ]
     }
 };
