f42af083a37748bbbb8bf4906217df484eed9185
Agus Hilman <gushil@gmail.com>
2019-12-09 21:33:46 +0700

    Addressing feedbacks

--- src/widget/text-print/text-print.js
+++ src/widget/text-print/text-print.js
@@ -10,25 +10,22 @@ class TextPrintWidget extends Widget {
      * @type string
      */
     static get selector() {
-        return 'input[type=text][data-type-xml="string"], textarea[data-type-xml="string"]';
+        return '.question > input[type=text]:not(.autocomplete):not(.ignore)';
     }
 
     _init() {
-        const printElement = document.createElement( 'div' );
+        this.question.classList.add( 'or-text-print-initialized' );
 
-        const elementTagName = this.element.tagName.toLowerCase();
-        let printElementClassName = 'print-' + elementTagName;
-        if ( elementTagName === 'input' ) {
-            printElementClassName += '-' + this.element.type;
-        }
-        printElement.className = printElementClassName + ' print-only';
+        const printElement = document.createElement( 'div' );
+        printElement.classList.add( this._getClassName() );
+        printElement.classList.add( 'print-only' );
 
         if ( getComputedStyle( this.element ).order !== '' ) {
             printElement.style.order = parseInt( getComputedStyle( this.element ).order, 10 ) + 1;
         }
         this.element.after( printElement );
 
-        const widget = this.element.parentElement.querySelector( '.' + printElementClassName );
+        const widget = this.element.parentElement.querySelector( `.${this._getClassName()}` );
         if ( this.element.value !== '' ) {
             const widgetText = this.element.value.replace( /\n/g, '<br>' );
             widget.innerHTML = widgetText;
@@ -39,6 +36,30 @@ class TextPrintWidget extends Widget {
             const widgetText = el.value.replace( /\n/g, '<br>' );
             widget.innerHTML = widgetText;
         } );
+    }
+
+    _getClassName() {
+        const elementTagName = this.element.tagName.toLowerCase();
+        let printElementClassName = 'print-' + elementTagName;
+        if ( elementTagName === 'input' ) {
+            printElementClassName += '-' + this.element.type;
+        }
+
+        return printElementClassName;
+    }
+
+    update() {
+        if ( this.element.getAttribute( 'data-calculate' ) !== null && this.element.getAttribute( 'data-calculate' ) !== '' ) {
+            const dataCalculateValue = this.element.getAttribute( 'data-calculate' ).trim();
+            const dataCalculateElement = document.querySelector( `[name="${dataCalculateValue}"]` );
+            if ( dataCalculateElement ) {
+                const widget = this.element.parentElement.querySelector( `.${this._getClassName()}` );
+                if ( dataCalculateElement.value !== '' ) {
+                    const widgetText = dataCalculateElement.value.replace( /\n/g, '<br>' );
+                    widget.innerHTML = widgetText;
+                }
+            }
+        }
     }
 }
 
