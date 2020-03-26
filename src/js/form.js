d56ab91f831bf4716f8ff6be16366b797221d94d
Martijn van de Rijdt <martijn@enketo.org>
2020-03-26 11:49:10 -0600

    changed: updated transformer

--- src/js/form.js
+++ src/js/form.js
@@ -1116,6 +1116,6 @@ Form.prototype.goToTarget = function( target ) {
  * @type string
  * @default
  */
-Form.requiredTransformerVersion = '1.38.1';
+Form.requiredTransformerVersion = '1.39.0';
 
 export { Form, FormModel };
