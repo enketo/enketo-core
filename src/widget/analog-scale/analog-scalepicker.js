0292b83453129316ba03850ab07d9e857a6f85b1
Martijn van de Rijdt <martijn@enketo.org>
2020-08-28 10:27:58 -0600

    added: vertical VAS appearance to show labels every 10 units
    https://github.com/OpenClinica/enketo-express-oc/issues/359
    
    
    Co-authored-by: Agus Hilman <gushil@gmail.com>

--- src/widget/analog-scale/analog-scalepicker.js
+++ src/widget/analog-scale/analog-scalepicker.js
@@ -133,11 +133,15 @@ class AnalogScaleWidget extends RangeWidget {
     get props() {
         const props = this._props;
         props.touch = support.touch;
-        props.min = isNumber( this.element.getAttribute( 'min' ) ) ? this.element.getAttribute( 'min' ) : 0;
-        props.max = isNumber( this.element.getAttribute( 'max' ) ) ? this.element.getAttribute( 'max' ) : 100;
-        props.step = isNumber( this.element.getAttribute( 'step' ) ) ? this.element.getAttribute( 'step' ) : 1; //( props.type === 'decimal' ? 0.1 : 1 );
         props.vertical = !props.appearances.includes( 'horizontal' );
         props.ticks = !props.appearances.includes( 'no-ticks' );
+        props.showScale = props.appearances.includes( 'show-scale' ) && props.vertical && props.ticks;
+        const min = isNumber( this.element.getAttribute( 'min' ) ) ? this.element.getAttribute( 'min' ) : 0;
+        const max = isNumber( this.element.getAttribute( 'max' ) ) ? this.element.getAttribute( 'max' ) : 100;
+        const step = isNumber( this.element.getAttribute( 'step' ) ) ? this.element.getAttribute( 'step' ) : ( props.showScale ? 10 : 1 ); //( props.type === 'decimal' ? 0.1 : 1 );
+        props.min = Number( min );
+        props.max = Number( max );
+        props.step = Number( step );
         props.maxTicks = 10;
 
         return props;
