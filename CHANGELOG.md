051538a7dc23e609ea7a478a1b37751648bd84c2
Martijn van de Rijdt <martijn@enketo.org>
2020-12-15 16:56:32 -0700

    fixed: the input field of a readonly question without a calculation but with a triggered setvalue/xforms-value-changed action remains hidden,
    https://github.com/OpenClinica/enketo-express-oc/issues/405

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ This project adheres to [Semantic Versioning](http://semver.org/).
 ##### Fixed
 - An exception occurs when a repeat is deleted.
 - When a calculation becomes non-relevant, values are sometimes cleared (they should stay).
+- The input field of a readonly question without a calculation but with a triggered setvalue/xforms-value-changed action remains hidden.
 
 [5.16.12] - 2020-12-04
 -----------------------
--- src/js/readonly.js
+++ src/js/readonly.js
@@ -2,8 +2,6 @@
  * @module readonly
  */
 
-import $ from 'jquery';
-
 export default {
     /**
      * Updates readonly
@@ -11,13 +9,17 @@ export default {
      * @param {UpdatedDataNodes} [updated] - The object containing info on updated data nodes.
      */
     update( updated ) {
-        const $nodes = this.form.getRelatedNodes( 'readonly', '', updated );
-        $nodes.each( function() {
-            $( this ).closest( '.question' ).addClass( 'readonly' );
+        const nodes = this.form.getRelatedNodes( 'readonly', '', updated ).get();
+        nodes.forEach( node =>  {
+            node.closest( '.question' ).classList.add( 'readonly' );
+
+            const path = this.form.input.getName( node );
+            const setValue = this.form.view.html.querySelector( `[data-setvalue][data-event="xforms-value-changed"][name="${path}"]` );
+
             // Note: the readonly-forced class is added for special readonly views of a form.
-            if ( !this.value && !this.dataset.calculate && !this.classList.contains( 'readonly-forced' ) ) {
-                this.classList.add( 'empty' );
-            }
+            const empty = !node.value && !node.dataset.calculate && !setValue && !node.classList.contains( 'readonly-forced' );
+
+            node.classList.toggle( 'empty', empty );
         } );
     }
 };
