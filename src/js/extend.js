561548a076f8b1d5219617356fc66c73b864c0b7
Martijn van de Rijdt <martijn@enketo.org>
2020-12-23 14:09:30 -0700

    Revert "changed: replaced XPath evaluator, closes #699"

--- src/js/extend.js
+++ src/js/extend.js
@@ -1,5 +1,9 @@
 // Extend native objects... Bad.
 
+// This import is just there so Alex and other XPath-evaluator-replacers get an automatic notification to extend the date object.
+// It is not required for those that use enketo-xpathjs
+import 'enketo-xpathjs/src/date-extensions';
+
 if ( typeof console.deprecate === 'undefined' ) {
     console.deprecate = ( bad, good ) => {
         console.warn( `${bad} is deprecated. Use ${good} instead.` );
