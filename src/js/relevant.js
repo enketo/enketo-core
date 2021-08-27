31993efd0977fdad0fa9a3abaf533f29010a2f2c
eyelidlessness <gnosis@gmail.com>
2021-08-27 14:58:09 -0700

    [Hacky] Fix nested repeats with relevance
    This is a targeted fix for one user, addressing a case where relevance on a nested/grouped repeat only works for the first top-level repeat. It causes a test failure for another use case, which provides helpful testing context and sort of hints at a likely direction for a more general fix.

--- src/js/relevant.js
+++ src/js/relevant.js
@@ -87,8 +87,9 @@ export default {
              * but currently has 0 repeats, the context will not be available. This same logic is applied in output.js.
              */
             let context = p.path;
-            if ( getChild( node, `.or-repeat-info[data-name="${p.path}"]` ) && !getChild( node,  `.or-repeat[name="${p.path}"]` ) ) {
-                context = null;
+            const repeatInfo = getChild( node, `.or-repeat-info[data-name="${p.path}"]` );
+            if ( repeatInfo != null && !getChild( node,  `.or-repeat[name="${p.path}"]` ) ) {
+                this.form.repeats.add( repeatInfo );
             }
 
             /*
